c9931586e548fcddf252bf3fe3e80c02c111b15a
VachetVirginie <vachetvirginie@gmail.com>
2025-11-19 20:51:13 +0100

    fix odeline size

--- w3c-validator-extension/content.js
+++
@@ -1,6 +0,0 @@
-// Content script pour récupérer le contenu HTML de la page
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'getHTML') {
-    sendResponse({html: document.documentElement.outerHTML});
-  }
-});
